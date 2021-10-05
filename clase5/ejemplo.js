73ecc098b17fc2ed47deb3e5f767b11aa7825a6b
DESKTOP-F62NSRA\Angela <angelaguerado1995@gmail.com>
2021-10-05 18:26:27 +0200

    modificacion ejemplo.js

--- clase5/ejemplo.js
+++ clase5/ejemplo.js
@@ -5,5 +5,29 @@ const operaciones = {
     dicision: (a,b) => a/b,
 };
 
-const key = 'resto';
+//extraer las keys de las operaciones
+console.log(object.keys(operaciones));
+const key = object.keys(operaciones)[Math.floor(Math.random()*object.keys(operaciones.lenght))];
 console.log(operaciones[key](7,3));
+
+//coger los valores de las operaciones de suma, resta, mult, div
+
+
+
+
+
+
+
+
+//ejemplo dos
+const persona = {
+    nombre: 'angela',
+    apellido: 'guerado',
+    edad: 25,
+    nombreCompleto: () => {
+//function (){//
+        return `${this.nombre} ${this.apellidos}`;
+    }
+};
+persona.nombreCompleto.bind(persona);
+console.log(persona.nombreCompleto());
