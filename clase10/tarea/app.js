75385bb4157852a977a111c83f4e8824d7299531
DESKTOP-F62NSRA\Angela <angelaguerado1995@gmail.com>
2021-10-28 19:32:12 +0200

    ejercicios clase 10

---
+++ clase10/tarea/app.js
@@ -0,0 +1,15 @@
+
+function salludar(){
+    console.log("Hola mundo loco");
+}
+
+const saludar = 'hola mundo loco';
+const miQueridaVariable = 'no se que hacer';
+
+module.exports = {
+    saludar,
+    miQueridaVariable,
+    salludar,
+    saludo: 'Hola',
+    numero: 6,
+}
