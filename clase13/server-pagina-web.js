7f865c4390abc6e96075ade03658b71c2890ff66
DESKTOP-F62NSRA\Angela <angelaguerado1995@gmail.com>
2021-12-16 18:33:56 +0100

    Clase 13 Actualizacion de los ejercicios

--- clase13/server-pagina-web.js
+++ clase13/server-pagina-web.js
@@ -1,1 +1,7 @@
 const http = requiere('http');
+const fs= requiere('fs');
+
+const serve = http.createserver((req, res)=>{
+    res.writeHead(200,{'content-type':'text/html'});
+    fs.createReadStream('./documento.html').pipe(res);
+}).listen(process.env.PORT || 3000);
