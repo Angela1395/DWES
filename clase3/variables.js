abdaaa942ac21d30f190345a31490eb73a973910
DESKTOP-F62NSRA\Angela <angelaguerado1995@gmail.com>
2021-12-18 18:42:50 +0100

    Actualizacion de nodejs actividad

--- clase3/variables.js
+++ clase3/variables.js
@@ -1,4 +1,15 @@
-variableGlobal = "global";
+/*variableGlobal = "global";
 var variableLocal = "local";
 let variableBloque = "Bloque";
-const constante = "constante";
+const constante = "constante";*/
+
+const numero = 7;
+for(let i = 0; i < numero; i++){
+    for(let j = numero - i; j>0; j--){
+        process.stdout.write("hola");
+    }
+    for(let j = 4; j<=i ; j++) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
